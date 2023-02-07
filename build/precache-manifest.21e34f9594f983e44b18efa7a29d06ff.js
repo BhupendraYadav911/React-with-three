d02503a8579faedddfe99516fab07f5e36b18203
mayank <mayank@digiprima.com>
2023-02-07 19:07:55 +0530

    creat new build

---
+++ build/precache-manifest.21e34f9594f983e44b18efa7a29d06ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070c1f5fc96c24c93f425c67e04c0968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee3c013793430f5083fd",
+    "url": "/static/css/main.12412808.chunk.css"
+  },
+  {
+    "revision": "b268b5f1291b9c7fa198",
+    "url": "/static/js/2.1d4d6ca3.chunk.js"
+  },
+  {
+    "revision": "9b81bfc72f3698726a50762eb8b87ca0",
+    "url": "/static/js/2.1d4d6ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3c013793430f5083fd",
+    "url": "/static/js/main.702bea37.chunk.js"
+  },
+  {
+    "revision": "c1fc5b63d1e8b5ac210e",
+    "url": "/static/js/runtime-main.40efd3d6.js"
+  }
+]);
