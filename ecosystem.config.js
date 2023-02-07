cdc07b31b864b07230f27e2ca41c472c9b4ee997
ghanshyam <ghanshyam.digiprima@gmail.com>
2023-02-07 15:05:37 +0530

    add ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,38 +15,5 @@ module.exports = {
         PM2_SERVE_SPA: 'true',
       },
     },
-  ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy : {
-    production : {
-      user: 'root',
-      host: '103.120.178.54',
-      ref: 'origin/master',
-      repo: 'git@github.com:Shubhras/react-with-three.git',
-      path: '/var/www/html/react-with-three',
-      // key: '/absolute/path/to/key',
-      // ssh_options: ['ForwardAgent=yes'],
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-      env: {
-        NODE_ENV: 'staging',
-        PORT: 3009,
-        URL: 'http://103.120.178.54',
-      },
-    },
-    staging: {
-      user: 'root',
-      host: '103.120.178.54',
-      ref: 'origin/master',
-      repo: 'git@github.com:Shubhras/react-with-three.git',
-      path: '/var/www/html/react-with-three',
-      // key: '/absolute/path/to/key',
-      // ssh_options: ['ForwardAgent=yes'],
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env staging'
-    },
-    dev : {}
-  }
+  ]
 };
